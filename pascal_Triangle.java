d659f1a1496e7d1db80193c59d32d33708f4868d
Manoj-1 <162041639+Manoj-eerabattini-1@users.noreply.github.com>
2025-05-18 15:43:16 +0530

    Create pascal_Triangle.java

---
+++ pascal_Triangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 1; i <= numRows; i++) {
+            list.add(generateRow(i));
+        }
+        return list;
+    }
+    public static List<Integer> generateRow(int row) {
+        List<Integer> li = new ArrayList<>();
+        int ans = 1;
+        li.add(ans);
+        for(int i = 1; i < row; i++) {
+            ans *= (row - i);
+            ans /= i;
+            li.add(ans);
+        }
+        return li;
+    }
+}
